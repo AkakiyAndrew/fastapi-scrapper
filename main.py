b4504c0563b85f69d11abba1d7d6ce0507960e9c
AkakiyAndrew <akakiy@email.ua>
2025-02-25 22:03:32 +0200

    basic web interface

--- main.py
+++ main.py
@@ -1,56 +1,22 @@
 import io
+import datetime
 from typing import Union
 import pathlib
 import scrapping.utils as utils
 import scrapping.scrapping as scrapping
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Body, Response
 from fastapi.responses import HTMLResponse, FileResponse, StreamingResponse
+from fastapi.staticfiles import StaticFiles
 from bson import ObjectId
 
-from models import *
+from models import Page, PageCollection, PageScrapeRequest
 from db import page_collection, static_collection
 
 app = FastAPI(
     title="Web Pages Saver API",
     summary="A sample application showing how to use FastAPI to add a ReST API to a MongoDB collection.",
 )
-
-
-@app.post(
-    "/pages/",
-    response_description="Add new page",
-    response_model=Page,
-    status_code=status.HTTP_201_CREATED,
-    response_model_by_alias=False,
-)
-async def create_page(page: Page = Body(...)):
-    """
-    Insert a new page record.
-
-    A unique `id` will be created and provided in the response.
-    """
-
-    # TODO: if provided ONLY a URL, fetch/scrap the page and save it.
-    # TODO: if provided a body - save it as is, fetch statics and save them as well.
-
-
-    if page.save_time is None:
-        page.save_time = datetime.datetime.now().astimezone()
-
-    # if not page.body and page.url:
-    #     presumptive_path = pathlib.Path(__file__).parent / "pages_example" / utils.get_domain(page.url)
-    #     if presumptive_path.exists():
-    #         with open(presumptive_path, mode="r", encoding="utf-8") as f:
-    #             page.body = f.read()
-    #     else:
-    #         page.body = "<!DOCTYPE html><html><head></head><body>Hello, World!</body></html>"
-    #     page.url = None
-
-    created_page_id = await scrapping.scrape_page(page.url, page.body)
-    
-    return await page_collection.find_one({"_id": created_page_id})
-
 
 @app.get(
     "/pages/",
@@ -67,6 +33,30 @@ async def list_pages():
     """
 
     return PageCollection(pages=await page_collection.find().to_list(100))
+
+@app.post(
+    "/pages/",
+    
+    response_description="Add new page",
+    response_model=Page,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
+async def create_page(page: PageScrapeRequest = Body(...)):
+    """
+    Insert a new page record.
+
+    A unique `id` will be created and provided in the response.
+    """
+
+    # page = {}
+
+    # if page.save_time is None:
+    #     page.save_time = datetime.datetime.now().astimezone()
+
+    created_page_id = await scrapping.scrape_page(page.url, page.body)
+    
+    return await page_collection.find_one({"_id": created_page_id})
 
 
 @app.get(
@@ -94,6 +84,9 @@ async def get_saved_static_file(static_id):
     static = await static_collection.find_one({"_id": ObjectId(static_id)})
     # return static['file']
     if not static:
-        return Response("Not found", status_code=404)
+        return Response("Not found", status_code=status.HTTP_404_NOT_FOUND)
 
     return StreamingResponse(content=io.BytesIO(static['file']), media_type=static["media_type"])
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static") # serve static files
--- models.py
+++ models.py
@@ -13,15 +13,47 @@ from bson import ObjectId
 # It will be represented as a `str` on the model so that it can be serialized to JSON.
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
+
+class PageScrapeRequest(BaseModel):
+    """
+    Request model for scraping a page.
+    """
+
+    url: str = Field(..., description="The URL of the page to scrape.")
+    body: Optional[str] = Field(
+        None,
+        description="The body of the page to save. If not provided, the page will be scraped.",
+    )
+    recursive: Optional[bool] = Field(
+        False, description="If true, scrape all URLs on page from same domain recursively."
+    )
+    statics_from_other_domains: Optional[bool] = Field(
+        False, description="If true, save statics from other domains."
+    )
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        json_encoders={ObjectId: str},
+        json_schema_extra={
+            "example": {
+                "url": "https://example.com/home",
+                "recursive": False,
+                "statics_from_other_domains": False,
+                "body": "<!DOCTYPE html><html><head></head><body>Hello, World!</body></html>",
+            }
+        },
+    )
+
 class Page(BaseModel):
     """
-    Single saved page (without statics)
+    Single saved page (body only)
     """
 
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     url: str = Field(...)
     body: Optional[str] = None
     save_time: Optional[NaiveDatetime] = None
+    title: Optional[str] = None
 
     model_config = ConfigDict(
         populate_by_name=True,
